1926d7885ea7ad2b5dedaad2e595cffa41a210ca
Yufei Li <yufeiminds@gmail.com>
2023-06-08 16:13:34 +0800

    chore: Remove generated acceptance testing

--- internal/testing/specs/iac/import/console/monitor/snapshot/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "monitor_ids" {
-  description = "The `guance_monitor`'s id."
-  value = [
-    guance_monitor.main_001.id,
-    guance_monitor.main_002.id,
-  ]
-}
--- internal/testing/specs/iac/import/grafana/snapshot/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    guance = {
-      source = "GuanceCloud/guance"
-    }
-  }
-}
